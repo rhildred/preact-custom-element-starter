9a740dd64ca8a8174aefce07a2970bf8e1e3d5ff
Richard Hildred <rhildred@gmail.com>
2025-03-27 01:17:54 +0000

    make static output files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "x-app.js",
+        assetFileNames: "x-app.[ext]",
+      },
+    }
+  }
 })
